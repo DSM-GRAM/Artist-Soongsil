185df0b0dfa983498615e5dd6ecc92e43416c838
PlanB <city7310@naver.com>
2017-10-16 00:01:34 +0900

    [UPDATE] affiliation field in UserModel

--- Server/db/models/user.py
+++ Server/db/models/user.py
@@ -5,4 +5,5 @@ class UserModel(Document):
     phone = StringField(primary_key=True)
     name = StringField(required=True)
     age = IntField(required=True)
+    affiliation = StringField(required=True)
     score = IntField(required=True)
