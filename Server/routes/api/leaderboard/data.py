5aa91bf1241b2c15c0b019513af129e9119f98ec
PlanB <city7310@naver.com>
2017-10-16 00:01:21 +0900

    [DONE] Leaderboard Information API, UserDrewImage API

---
+++ Server/routes/api/leaderboard/data.py
@@ -0,0 +1,21 @@
+from flask import send_from_directory
+from flask_restful_swagger_2 import Resource, request, swagger
+
+from db.models.user import UserModel
+
+
+class Information(Resource):
+    def get(self):
+        return [{
+            'name': user.name,
+            'phone': user.phone,
+            'affiliation': user.affiliation,
+            'score': user.score
+        } for user in UserModel.objects], 200
+
+
+class UserDrewImage(Resource):
+    def get(self):
+        phone = request.args.get('phone')
+
+        return send_from_directory('user_images', '{0}.png'.format(phone))
