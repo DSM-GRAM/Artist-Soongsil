9aec0830d67d9c0c6672018e61359a85c16fbfc1
PlanB <city7310@naver.com>
2017-10-17 08:17:37 +0900

    [DONE] Device API Swagger

--- Server/routes/api/shower/device.py
+++ Server/routes/api/shower/device.py
@@ -1,9 +1,11 @@
 from flask_restful_swagger_2 import Resource, request, swagger
 
 from db.models.device import DeviceModel
+from routes.api.shower import device_doc
 
 
 class Device(Resource):
+    @swagger.doc(device_doc.DEVICE)
     def post(self):
         registration_id = request.form.get('registration_id')
 
