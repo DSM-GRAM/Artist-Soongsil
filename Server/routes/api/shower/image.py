f4fd33415d8ed2d64272a1f5c53cc0fd7e97ee77
PlanB <city7310@naver.com>
2017-10-17 08:17:50 +0900

    [DONE] Sample image API Swagger

--- Server/routes/api/shower/image.py
+++ Server/routes/api/shower/image.py
@@ -1,8 +1,11 @@
 from flask import send_from_directory
 from flask_restful_swagger_2 import Resource, request, swagger
 
+from routes.api.shower import image_doc
 
-class Image(Resource):
+
+class SampleImage(Resource):
+    @swagger.doc(image_doc.SAMPLE_IMAGE)
     def get(self):
         image_name = request.args.get('image_name')
 
